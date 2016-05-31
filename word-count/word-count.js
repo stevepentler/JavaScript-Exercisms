52afc77d29f7e20b063748c09533b823484b453d
Steve Pentler <stevepentler@gmail.com>
2016-05-30 23:25:39 -0500

    word count JS broken

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -1,6 +1,20 @@
 "use strict";
 
 function Words() {
+
+  this.tally = {};
+
+  this.count = function(phrase) {
+    var words_array = phrase.split(" ");
+    words_array.forEach(word, index) {
+      var word = word.downcase;
+      if (this.tally.word) {
+        return this.tally.word += 1;
+      } else {
+        return this.tally.word = 1;
+      }
+    }
+  }
 }
 
 module.exports = Words
