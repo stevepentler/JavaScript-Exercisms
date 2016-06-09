02b9577f120cb9cca5414433312432ccf3132629
Steve Pentler <stevepentler@gmail.com>
2016-06-08 21:30:29 -0500

    Starts sum of multiples, currently unable to set property on instance to track multiples

---
+++ sum-of-multiples/sum-of-multiples.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function SumOfMultiples(multiples) {
+  this.multiples = multiples;
+};
+
+SumOfMultiples.prototype = {
+  to: function(max) {
+  }
+}
+module.exports = SumOfMultiples;
