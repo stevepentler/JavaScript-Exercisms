c801929619e35a88bf2d0597c1d9c74ce3b472c5
Steve Pentler <stevepentler@gmail.com>
2016-06-09 15:53:13 -0600

    Passes all tests

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -2,15 +2,21 @@
 
 function DnaTranscriber() {
   this.translations = {
-    'C': 'G',
-    'G': 'C',
-    'A': 'U',
-    'T': 'A'
+    C: 'G',
+    G: 'C',
+    A: 'U',
+    T: 'A'
   }
 };
 
 DnaTranscriber.prototype = {
-  toRna: function(nucleotide) {
+
+
+  toRna: function(strand) {
+    return strand.split("").map(this.translate.bind(this)).join("");
+  },
+
+  translate: function(nucleotide) {
     return this.translations[nucleotide];
   }
 }
