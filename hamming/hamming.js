b44c458d6b3c18429a912fac6fce11fb64223407
Steve Pentler <stevepentler@gmail.com>
2016-06-08 20:40:54 -0500

    Passes first test using forEach

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -2,6 +2,18 @@
 
 function Hamming() {};
 
+Hamming.prototype = {
+  compute: function(string1, string2) {
+    let difference = 0
+    Array.from(string1).forEach(function(char, index) {
+      if (char !== Array.from(string2)[index]) {
+        difference += 1
+      }
+    })
+    return difference
+  }
+}
+
 
 
 module.exports = Hamming;
