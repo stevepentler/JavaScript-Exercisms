e5f4cdbec804aced312aa50be9f42f1aaf1467f4
Steve Pentler <stevepentler@gmail.com>
2016-06-08 21:10:35 -0500

    Extrapolates compareStrings, adds error message

--- hamming/hamming.js
+++ hamming/hamming.js
@@ -4,16 +4,22 @@ function Hamming() {};
 
 Hamming.prototype = {
   compute: function(string1, string2) {
-    let difference = 0
-    Array.from(string1).forEach(function(char, index) {
-      if (char !== Array.from(string2)[index]) {
-        difference += 1
-      }
-    })
-    return difference
+    if (string1.length === string2.length) {
+      return compareString(string1, string2)
+    } else {
+      throw new Error('DNA strands must be of equal length.')
+    }
   }
 }
 
-
+function compareString(string1, string2) {
+  let difference = 0
+  Array.from(string1).forEach(function(char, index) {
+    if (char !== Array.from(string2)[index]) {
+      difference += 1
+    }
+  })
+  return difference
+}
 
 module.exports = Hamming;
